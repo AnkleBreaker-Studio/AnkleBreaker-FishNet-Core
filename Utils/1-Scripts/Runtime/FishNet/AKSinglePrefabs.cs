8a1f9cef7fad194778a528124d375c6e2780ea39
TakeruGrima <timothee.le.corre@grimasgames.fr>
2026-03-05 15:19:09 +0100

    add Utils/1-Scripts/Runtime/FishNet/AKSinglePrefabs.cs

---
+++ Utils/1-Scripts/Runtime/FishNet/AKSinglePrefabs.cs
@@ -0,0 +1,13 @@
+using FishNet.Managing.Object;
+using UnityEngine;
+
+namespace AnkleBreaker.Utils.Fishnet
+{
+    public class AKSinglePrefabs : SinglePrefabObjects
+    {
+        public override void RemoveNull()
+        {
+            base.RemoveNull();
+        }
+    }
+}
